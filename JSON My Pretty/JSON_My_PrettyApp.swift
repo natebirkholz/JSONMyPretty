0d8197ae9c5e51da056ff5b75f2df39a6cded950
Nate Birkholz <nbirkholz@gmail.com>
2023-11-18 11:27:58 -0700

    Initial Commit

---
+++ JSON My Pretty/JSON_My_PrettyApp.swift
@@ -0,0 +1,13 @@
+// JSON_My_PrettyApp.swift
+// Created by Nate Birkholz
+
+import SwiftUI
+
+@main
+struct JSON_My_PrettyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
